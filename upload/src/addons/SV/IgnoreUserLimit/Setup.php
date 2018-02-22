4b0ad9be747610a3140fcac96cc0d6f8f09e154a
Xon <tazipper1@hotmail.com>
2018-02-22 19:42:17 +0800

    Update installer to use Entity User group constant

--- upload/src/addons/SV/IgnoreUserLimit/Setup.php
+++ upload/src/addons/SV/IgnoreUserLimit/Setup.php
@@ -7,6 +7,7 @@ use XF\AddOn\StepRunnerInstallTrait;
 use XF\AddOn\StepRunnerUninstallTrait;
 use XF\AddOn\StepRunnerUpgradeTrait;
 use XF\Db\Schema\Alter;
+use XF\Entity\User;
 
 class Setup extends AbstractSetup
 {
@@ -14,15 +15,10 @@ class Setup extends AbstractSetup
 	use StepRunnerUpgradeTrait;
 	use StepRunnerUninstallTrait;
 
-    public static $defaultGuestGroupId = 1;
-    public static $defaultRegisteredGroupId = 2;
-    public static $defaultAdminGroupId = 3;
-    public static $defaultModeratorGroupId = 4;
-
 	public function installStep1()
 	{
         $this->db()->query("insert ignore into xf_permission_entry (user_group_id, user_id, permission_group_id, permission_id, permission_value, permission_value_int)
-                select " . self::$defaultRegisteredGroupId . ", 0, 'general', 'sv_userIgnoreLimit', 'allow', '-1'
+                select " . User::GROUP_REG . ", 0, 'general', 'sv_userIgnoreLimit', 'allow', '-1'
                 from xf_permission_entry
             ");
 
