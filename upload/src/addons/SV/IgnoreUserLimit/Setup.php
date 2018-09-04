959d6e5feca3e13f693aad23031d67b8da047e1b
Xon <tazipper1@hotmail.com>
2018-09-05 01:07:28 +0800

    Use shared installer code

--- upload/src/addons/SV/IgnoreUserLimit/Setup.php
+++ upload/src/addons/SV/IgnoreUserLimit/Setup.php
@@ -2,32 +2,23 @@
 
 namespace SV\IgnoreUserLimit;
 
+use SV\Utils\InstallerHelper;
 use XF\AddOn\AbstractSetup;
 use XF\AddOn\StepRunnerInstallTrait;
 use XF\AddOn\StepRunnerUninstallTrait;
 use XF\AddOn\StepRunnerUpgradeTrait;
-use XF\Db\Schema\Alter;
 use XF\Entity\User;
 
 class Setup extends AbstractSetup
 {
+    // from https://github.com/Xon/XenForo2-Utils cloned to src/addons/SV/Utils
+    use InstallerHelper;
 	use StepRunnerInstallTrait;
 	use StepRunnerUpgradeTrait;
 	use StepRunnerUninstallTrait;
 
 	public function installStep1()
 	{
-        $this->db()->query("insert ignore into xf_permission_entry (user_group_id, user_id, permission_group_id, permission_id, permission_value, permission_value_int)
-                select " . User::GROUP_REG . ", 0, 'general', 'sv_userIgnoreLimit', 'allow', '-1'
-                from xf_permission_entry
-                limit 1
-            ");
-
-        $this->app->jobManager()->enqueueUnique(
-            'permissionRebuild',
-            'XF:PermissionRebuild',
-            [],
-            false
-        );
+	    $this->applyGlobalPermissionIntForGroup( 'general', 'sv_userIgnoreLimit', -1, User::GROUP_REG);
 	}
 }
